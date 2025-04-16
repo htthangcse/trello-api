d46e2d3510ca1216b111ad7a9025cd6db3985698
htthangcse <hatienthangk22cse@gmail.com>
2025-04-16 22:10:31 +0700

    express router

--- src/routes/v1/boardRoutes.js
+++ src/routes/v1/boardRoutes.js
@@ -6,16 +6,15 @@
 
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
-import { boardRoutes } from '~/routes/v1/boardRoutes'
 
 const Router = express.Router()
 
-// Check api v1/status
-Router.get('/status', (req, res) => {
-  res.status(StatusCodes.OK).json({ message: 'APIs V1 are ready to use.' })
-})
+Router.route('/')
+  .get((req, res) => {
+    res.status(StatusCodes.OK).json({ message: 'GET: API get list boards' })
+  })
+  .post((req, res) => {
+    res.status(StatusCodes.CREATED).json({ message: 'POST: API create new board' })
+  })
 
-// Board APIs
-Router.use('/boards', boardRoutes)
-
-export const APIs_V1 = Router
+export const boardRoutes = Router
