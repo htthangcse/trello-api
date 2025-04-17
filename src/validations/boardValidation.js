4d97769fd44a45bc7b5e8ee805580ae378a31c8f
htthangcse <hatienthangk22cse@gmail.com>
2025-04-17 11:51:32 +0700

    error handling

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -6,6 +6,7 @@
 
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   /**
@@ -32,10 +33,7 @@ const createNew = async (req, res, next) => {
     // Validate dữ liệu xong xuôi hợp lệ thì cho request đi tiếp sang Controller
     next()
   } catch (error) {
-    console.log(error)
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message
-    })
+    next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
   }
 }
 
